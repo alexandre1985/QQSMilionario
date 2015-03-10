36b531a15a1580d24957bcfb02e55ba1075f4887
alexandre1985 <dannysantos1985@gmail.com>
2015-03-10 13:30:55 +0000

    a fazer os recordes

--- qqsm/OJogo.java
+++ qqsm/OJogo.java
@@ -1,4 +1,8 @@
 import java.util.Random;
+import java.util.Scanner;
+import java.net.URL;
+import org.apache.commons.net.ftp.FTPClient;
+import java.io.*;
 
 /**
  * Esta classe trata do nível e define todos métodos principais do jogo.
@@ -10,6 +14,9 @@ public class OJogo
 {
     BancoDeDados questoes;
     int nivel=1;
+    final String ftpURL = "ftp.byethost7.com";
+    final String ficRecordes = "recordes.txt";
+    FTPClient client;
     
     public OJogo()
     {
@@ -63,5 +70,76 @@ public class OJogo
         if( nivel == 15)
             return true;
         return false;
+    }
+    
+    public boolean eRecorde() throws Exception
+    {
+        openFTPSession();
+        downloadRecordes();
+        Scanner s = new Scanner(new File(ficRecordes));
+        String ultimo = null;
+        while(s.hasNext()) {
+            ultimo = s.next();
+        }
+        
+        return false;
+    }
+    
+    public void openFTPSession()
+    {
+        try {
+            client = new FTPClient();
+            client.connect(ftpURL);
+            client.login("b7_15960406", "qqsmilionario");
+            client.changeWorkingDirectory("htdocs");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void closeFTPSession()
+    {
+        try {
+            client.logout();
+            client.disconnect();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void downloadRecordes() throws Exception
+    {
+        FileOutputStream fos = new FileOutputStream(ficRecordes);
+        client.retrieveFile(ficRecordes, fos);
+        fos.close();
+    }
+    
+    public void uploadRecordes()
+    {
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream(ficRecordes);
+            
+            // Store file to server
+            client.storeFile(ficRecordes, fis);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (fis != null) {
+                    fis.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    
+    public void deleteRecordes() throws Exception
+    {
+        openFTPSession();
+        client.deleteFile(ficRecordes);
+        client.logout();
+        client.disconnect();
     }
 }
